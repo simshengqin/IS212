88f21e7fc14d2ae976bf45bad25949d87677ce13
Carecci Vittorio Wei Long <vcarecci.2018@sis.smu.edu.sg>
2019-09-29 17:29:22 +0800

    Create CourseCompleted.php
    Co-Authored-By: simshengqin <simshengqin@users.noreply.github.com>

---
+++ app/include/CourseCompleted.php
@@ -0,0 +1,23 @@
+<?php
+
+class Course {
+    private $userid;
+    private $course;
+
+    public function __construct($userid, $course) {
+        $this->userid = $userid;
+        $this->course = $course;
+    }
+
+    public function getUserid() {
+        return $this->userid;
+    }
+    public function getCourse() {
+        return $this->course;
+    }
+
+}
+
+
+
+?>
