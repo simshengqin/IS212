99b1df8b89a805ca38ea84b6c057ed1aaf712f28
Brendon <brendon.lim.2018@sis.smu.edu.sg>
2019-10-10 11:44:08 +0800

    added json for student/admin authentication
    Co-Authored-By: matthewtan96 <matthewtan96@users.noreply.github.com>

---
+++ app/json/authenticate_form.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="include/style.css">
+    </head>
+    <body>
+        <h1>Form for testing JSON authenticate</h1>
+        <form method='POST' action='authenticate.php'>
+            <table>
+                <tr>
+                    <td>User ID</td>
+                    <td>
+                        <input name='userid' value='ben.ng.2009' />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <input name='password' type='password' value='qwerty129' />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan='2'>
+                        <input name='Login' type='submit' />
+                    </td>
+                </tr>
+            </table>             
+        </form>
+
+    </body>
+</html>
