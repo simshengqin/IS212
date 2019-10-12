0844135ea77821f59f5881042979cc07176243af
Carecci Vittorio Wei Long <vcarecci.2018@sis.smu.edu.sg>
2019-10-12 15:42:16 +0800

    Update the Dropbid page with Bootstrap design
    Co-Authored-By: Brendon <brendonlch@users.noreply.github.com>
    Co-Authored-By: matthewtan96 <matthewtan96@users.noreply.github.com>

--- app/dropBid.php
+++ app/dropBid.php
@@ -25,41 +25,91 @@
           
       }
   }
-
-
-  #Creating table of current bids
-
-  echo "
-  <form action = 'dropBid.php' method = 'POST'>
-  <table border = 1>
-    <tr>
-    <th> Course ID </th>
-    <th> Section Number </th>
-    <th> Bid amount </th>
-    <th> Drop bid? </th>
-    </tr>
-  ";
-
-  foreach ($bids as $bid){
-      $code = $bid->getCode();
-      $section_number = $bid->getSection();
-      $bid_amount = $bid->getAmount();
-
-      echo "<tr> 
-      <td> $code  </td>
-      <td> $section_number </td>
-      <td> $bid_amount </td>
-      <td> <input type='checkbox' name = '$code' value = '$code'></td>
-      </tr> ";
-  }
-  echo "</table>";
-  echo "
-  <input type='submit' value='Drop bids'>
-  </form>";
-
-
-    
-
-
-
 ?>
+
+  
+
+  <html>
+
+  <head>
+    <title> drop bids </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </head>
+
+  <div class="container">
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+      <!-- Brand/logo -->
+      <a class="navbar-brand" href="#">BIOS</a>
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="landingPage.php"> HOME </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="addBidPage.php"> ADD BID(s)</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dropBid.php"> DROP BID(s)</a>
+        </li>
+      </ul>
+    </nav>
+    <!-- End of Navigation Bar -->
+
+  <!-- Display all of student's bids-->
+  <div class = "row">
+   <div class="col-sm-12" style='margin-top: 15vh'>
+    <form action = 'dropBid.php' method = 'POST'>
+      <table class="table table-striped">
+        <h3> Your Bid(s) </h3>
+        <thead>
+          <tr>
+            <th> Course ID </th>
+            <th> Section Number </th>
+            <th> Bid amount </th>
+            <th> Drop bid? </th>
+          </tr>
+          </thead>
+          <tbody>
+            <?php
+              if(count($bids) == 0)
+              {
+                echo"<tr> <td colspan='4'> <h4 style='text-align: center;'> You currently have no bids </h4> </td> </tr>";
+              }
+              else
+              {
+                foreach ($bids as $bid){
+                  $code = $bid->getCode();
+                  $section_number = $bid->getSection();
+                  $bid_amount = $bid->getAmount();
+                  echo "<tr> 
+                    <td> $code  </td>
+                    <td> $section_number </td>
+                    <td> $bid_amount </td>
+                    <td> <input type='checkbox' name = '$code' value = '$code'></td>
+                  </tr> ";
+                }
+              }
+            ?>
+          </tbody>
+      </table>
+      <input type='submit' class="btn btn-primary" value='Drop Bid(s)'>
+    </form>
+   </div>
+  </div>
+
+  </div>
+
+ 
+
+
+
+
+
+
+
