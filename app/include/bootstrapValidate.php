65160cd0545569066c69d1c21b8744aaa63aa727
simshengqin <sqsim.2018@sis.smu.edu.sg>
2019-10-16 21:50:18 +0800

    Removed the unnecessary elseif
    Co-Authored-By: chadaeun04 <chadaeun04@users.noreply.github.com>

--- app/include/bootstrapValidate.php
+++ app/include/bootstrapValidate.php
@@ -114,7 +114,6 @@ function validateSection($section_data, $row, $allCourseInfo){
         if (!preg_match("/^[S](\d?[1-9]|[1-9]0)$/", $section)){       // Check if first character have a 'S' followed by 1-99
             $message[] = "invalid section";
         }
-        elseif 
     }
 
     // Section Day Validation (check between 1 to 7)
